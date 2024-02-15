78efcac6bfefed671038dca4274933b349ce7103
SonWooJu <sksthsdnwn23@gmail.com>
2024-02-15 23:28:16 +0900

    forjs

--- tab.js
+++ tab.js
@@ -1,19 +1,31 @@
-$('.tab-button').eq(0).on('click',function(){
-$('.tab-button').removeClass('orange');
-$('.tab-button').eq(0).addClass('orange');
-$('.tab-content').removeClass('show');
-$('.tab-content').eq(0).addClass('show');
-})
+var button = $('.tab-button');
+var content = $('.tab-content');
 
-$('.tab-button').eq(1).on('click',function(){
-    $('.tab-button').removeClass('orange');
-    $('.tab-button').eq(1).addClass('orange');
-    $('.tab-content').removeClass('show');
-    $('.tab-content').eq(1).addClass('show');
-    })
- $('.tab-button').eq(2).on('click',function(){
-     $('.tab-button').removeClass('orange');
-    $('.tab-button').eq(2).addClass('orange');
-    $('.tab-content').removeClass('show');
-    $('.tab-content').eq(2).addClass('show');
-    })
+for(let i = 0; i<3; i++){
+    $('.tab-button').eq(i).on('click',function(){
+        $('.tab-button').removeClass('orange');
+        $('.tab-button').eq(i).addClass('orange');
+        $('.tab-content').removeClass('show');
+        $('.tab-content').eq(i).addClass('show');
+        })
+}
+
+// $('.tab-button').eq(0).on('click',function(){
+// $('.tab-button').removeClass('orange');
+// $('.tab-button').eq(0).addClass('orange');
+// $('.tab-content').removeClass('show');
+// $('.tab-content').eq(0).addClass('show');
+// })
+
+// $('.tab-button').eq(1).on('click',function(){
+//     $('.tab-button').removeClass('orange');
+//     $('.tab-button').eq(1).addClass('orange');
+//     $('.tab-content').removeClass('show');
+//     $('.tab-content').eq(1).addClass('show');
+//     })
+//  $('.tab-button').eq(2).on('click',function(){
+//      $('.tab-button').removeClass('orange');
+//     $('.tab-button').eq(2).addClass('orange');
+//     $('.tab-content').removeClass('show');
+//     $('.tab-content').eq(2).addClass('show');
+//     })
