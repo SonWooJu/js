f6a223ae4cf7899735e3fd4affbfce039e6fec78
SonWooJu <sksthsdnwn23@gmail.com>
2024-02-23 23:44:21 +0900

    1event

--- tab.js
+++ tab.js
@@ -1,11 +1,27 @@
 var button = $('.tab-button');
 var content = $('.tab-content');
 
-for(let i = 0; i<3; i++){
-    $('.tab-button').eq(i).on('click',function(){
-        topen(i)
-        })
-}
+$('.list').click(function(e){
+    if(e.target == document.querySelectorAll('.tab-button')[0]) {
+        topen(0)
+
+    }
+    else if(e.target == document.querySelectorAll('.tab-button')[1]) {
+        topen(1)
+
+    }
+    else if(e.target == document.querySelectorAll('.tab-button')[2]) {
+        topen(2)
+
+    }
+
+})
+
+// for(let i = 0; i<3; i++){
+//     $('.tab-button').eq(i).on('click',function(){
+//         topen(i)
+//         })
+// }
 function topen(num){
     $('.tab-button').removeClass('orange');
     $('.tab-button').eq(num).addClass('orange');
